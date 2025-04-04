cf7103f522ddc3118e50bf3e7e66abdaea8555f0
Justin Cheung <superpotato1208@gmail.com>
2025-04-04 15:54:35 -0700

    button made

---
+++ src/app/_components/GenerateHeatmapButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function GenerateHeatmapButton() {
+  return (
+    <button className="bg-[#2e2e2e] text-white px-4 py-2 rounded-md">
+      Generate Heatmap
+    </button>
+  );
+}
