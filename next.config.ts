165a64dcd9ffd214016bfb16d9a1b617e46d9ff7
Justin Cheung <superpotato1208@gmail.com>
2025-04-06 17:52:35 -0700

    Add Hotreload for Windows10/MACOS

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  webpack: (config: any, { dev }: { dev: boolean }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-        ignored: /node_modules/,
-      };
-    }
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
