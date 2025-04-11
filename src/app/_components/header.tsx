3959794903680fe8cd8a4fa9668cde08b75d4acb
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-10 18:01:58 -0700

    Autoformatter

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,12 +1,10 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import { FaDiscord, FaLinkedin } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between px-6 py-4 bg-white border-b">
+    <header className="flex justify-between items-center py-4 px-6 bg-white border-b">
       {/* Logo */}
       <div className="flex items-center space-x-4">
         <Image
@@ -25,17 +23,18 @@ export default function Header() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FaLinkedin className="w-6 h-6 text-red-600 hover:text-red-800 transition-colors" />
+          <FaLinkedin className="w-6 h-6 text-red-600 transition-colors hover:text-red-800" />
         </Link>
         <Link
           href="https://discord.gg/ekb49NsP" //TPC Discord Server Link
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FaDiscord className="w-6 h-6 text-red-600 hover:text-red-800 transition-colors" />
+          <FaDiscord className="w-6 h-6 text-red-600 transition-colors hover:text-red-800" />
         </Link>
       </div>
     </header>
   );
 }
 //
+
