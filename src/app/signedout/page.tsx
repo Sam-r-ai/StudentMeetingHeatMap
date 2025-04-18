20bccdbde4c67a51860c4f330c198ad0967c9f9a
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-17 21:09:25 -0700

    Sign in page uses Google icon

--- src/app/signedout/page.tsx
+++ src/app/signedout/page.tsx
@@ -1,13 +1,26 @@
-import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import SignInButton from "./_components/SignInButton";
 
 export default function SignedOutPage() {
   return (
-    <div className="flex flex-col justify-center items-center gap-1 text-xl mt-16">
-      <h1 className="text-4xl">Sorry!</h1>
-      <p>
-      You need to sign in with your CSU Chico Google account first!
-      </p>
-      <Button>Sign In</Button>
+    <div className="flex justify-center items-center mt-24">
+      <Card className="flex flex-col justify-center w-1/2 h-[25dvh]">
+        <CardHeader className="text-center">
+          <CardTitle className="text-4xl">Please Sign In!</CardTitle>
+          <CardDescription className="text-2xl">
+            Sign into your <strong>CSU Chico Google account</strong> to continue.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex justify-center items-center">
+          <SignInButton />
+        </CardContent>
+      </Card>
     </div>
   );
 }
