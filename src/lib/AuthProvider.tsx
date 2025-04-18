27399117ffe7d6778bca06268afab8c98323ffe0
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-17 21:09:35 -0700

    AuthProvider has loading and whitelist

--- src/lib/AuthProvider.tsx
+++ src/lib/AuthProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function AuthProvider({
@@ -8,14 +8,24 @@ export default function AuthProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const router = useRouter();
+  const pathname = usePathname();
+
+  const allowedPaths = ["/signedout"];
 
   useEffect(() => {
+    if (status === "loading") return;
     if (!session || !session.user?.email?.endsWith("csuchico.edu")) {
       router.push("/signedout");
     }
-  }, [session, router]);
+  }, [session, status, router]);
 
-  return <>{children}</>;
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "authenticated" || allowedPaths.includes(pathname)) {
+    return <>{children}</>;
+  }
 }
