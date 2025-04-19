611a4a8d2f3f51c62fab0b33f5e7d36ec3eddaed
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-19 16:58:15 -0700

    Ignores <Link> lint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      /* Don't want to go into Vercel limits for <Link> */
+      "@next/next/no-html-link-for-pages": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
